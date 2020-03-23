5beb19d48434b541580e84838a820f3a052981fb
fakorede <abiolafakorede@gmail.com>
2020-03-23 21:30:22 +0100

    handling validation errors

--- app/Http/Controllers/FriendRequestResponseController.php
+++ app/Http/Controllers/FriendRequestResponseController.php
@@ -10,24 +10,24 @@ use Illuminate\Http\Request;
 
 class FriendRequestResponseController extends Controller
 {
-    public function store() 
+    public function store()
     {
         $data = request()->validate([
-            'user_id' => '',
-            'status' => '',
+            'user_id' => 'required',
+            'status' => 'required',
         ]);
 
         try {
             $friendRequest = Friend::where('user_id', $data['user_id'])
-            ->where('friend_id', auth()->user()->id)
-            ->firstOrFail();
-        } catch(ModelNotFoundException $e) {
+                ->where('friend_id', auth()->user()->id)
+                ->firstOrFail();
+        } catch (ModelNotFoundException $e) {
             throw new FriendRequestNotFoundException();
         }
 
-            $friendRequest->update(array_merge($data, [
-                'confirmed_at' => now()
-            ]));
+        $friendRequest->update(array_merge($data, [
+            'confirmed_at' => now(),
+        ]));
 
         return new FriendResource($friendRequest);
     }
