fb312b0ef3f8d4f13fb41876a9b63a7962b4a97b
fakorede <abiolafakorede@gmail.com>
2020-03-25 21:50:19 +0100

    Accepting and ignoring friend requests

--- resources/js/store/modules/profile.js
+++ resources/js/store/modules/profile.js
@@ -13,9 +13,17 @@ const getters = {
     friendButtonText: (state, getters, rootState) => {
         if (getters.friendship === null) {
             return "Add Friend";
-        } else if (getters.friendship.data.attributes.confirmed_at === null) {
+        } else if (
+            getters.friendship.data.attributes.confirmed_at === null &&
+            getters.friendship.data.attributes.friend_id !==
+                rootState.User.user.data.user_id
+        ) {
             return "Friend Request Pending";
+        } else if (getters.friendship.data.attributes.confirmed_at !== null) {
+            return "";
         }
+
+        return "Accept";
     }
 };
 
@@ -34,12 +42,31 @@ const actions = {
             });
     },
     sendFriendRequest({ commit, state }, friendId) {
-        commit("setButtonText", "Loading");
-
         axios
             .post("/api/friend-request", { friend_id: friendId })
             .then(res => {
                 commit("setUserFriendship", res.data);
+            })
+            .catch(err => {});
+    },
+    acceptFriendRequest({ commit, state }, userId) {
+        axios
+            .post("/api/friend-request-response", {
+                user_id: userId,
+                status: 1
+            })
+            .then(res => {
+                commit("setUserFriendship", res.data);
+            })
+            .catch(err => {});
+    },
+    ignoreFriendRequest({ commit, state }, userId) {
+        axios
+            .delete("/api/friend-request-response/delete", {
+                data: { user_id: userId }
+            })
+            .then(res => {
+                commit("setUserFriendship", null);
             })
             .catch(err => {});
     }
@@ -54,9 +81,6 @@ const mutations = {
     },
     setUserStatus(state, status) {
         state.userStatus = status;
-    },
-    setButtonText(state, text) {
-        state.friendButtonText = text;
     }
 };
 
--- resources/js/views/Users/Show.vue
+++ resources/js/views/Users/Show.vue
@@ -1,83 +1,103 @@
 <template>
-    <div class="flex flex-col items-center">
-        <div class="relative mb-8">
-            <div class="w-100 h-64 overflow-hidden z-10">
-                <img
-                    src="https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                    alt="user background image"
-                    class="object-cover w-full"
-                />
-            </div>
+  <div class="flex flex-col items-center">
+    <div class="relative mb-8">
+      <div class="w-100 h-64 overflow-hidden z-10">
+        <img
+          src="https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          alt="user background image"
+          class="object-cover w-full"
+        />
+      </div>
 
-            <div
-                class="absolute flex items-center bottom-0 left-0 -mb-8 ml-12 z-20"
-            >
-                <div class="w-32">
-                    <img
-                        src="https://cdn.pixabay.com/photo/2014/07/09/10/04/man-388104_960_720.jpg"
-                        alt="user profile image"
-                        class="object-cover w-32 h-32 border-4 border-gray-200 rounded-full shadow-lg"
-                    />
-                </div>
-                <p class="text-2xl text-gray-100 ml-4">
-                    {{ user.data.attributes.name }}
-                </p>
-            </div>
+      <div class="absolute flex items-center bottom-0 left-0 -mb-8 ml-12 z-20">
+        <div class="w-32">
+          <img
+            src="https://cdn.pixabay.com/photo/2014/07/09/10/04/man-388104_960_720.jpg"
+            alt="user profile image"
+            class="object-cover w-32 h-32 border-4 border-gray-200 rounded-full shadow-lg"
+          />
+        </div>
+        <p class="text-2xl text-gray-100 ml-4">
+          {{ user.data.attributes.name }}
+        </p>
+      </div>
 
-            <div
-                class="absolute flex items-center bottom-0 right-0 mb-4 mr-12 z-20"
-            >
-                <button
-                    v-if="friendButtonText"
-                    class="py-1 px-3 bg-gray-400 rounded"
-                    @click="
+      <div class="absolute flex items-center bottom-0 right-0 mb-4 mr-12 z-20">
+        <button
+          v-if="friendButtonText && friendButtonText !== 'Accept'"
+          class="py-1 px-3 bg-gray-400 rounded"
+          @click="
                         $store.dispatch(
                             'sendFriendRequest',
                             $route.params.userId
                         )
                     "
-                >
-                    {{ friendButtonText }}
-                </button>
-            </div>
-        </div>
-
-        <p v-if="loading">Loading posts...</p>
-
-        <Post
-            v-else
-            v-for="post in posts.data"
-            :key="post.data.post_id"
-            :post="post"
-        />
-
-        <p v-if="!loading && posts.data.length < 1">No posts found!</p>
+        >
+          {{ friendButtonText }}
+        </button>
+        <button
+          v-if="friendButtonText && friendButtonText === 'Accept'"
+          class="mr-2 py-1 px-3 bg-blue-500 rounded"
+          @click="
+                        $store.dispatch(
+                            'acceptFriendRequest',
+                            $route.params.userId
+                        )
+                    "
+        >
+          Accept
+        </button>
+        <button
+          v-if="friendButtonText && friendButtonText === 'Accept'"
+          class="py-1 px-3 bg-gray-400 rounded"
+          @click="
+                        $store.dispatch(
+                            'ignoreFriendRequest',
+                            $route.params.userId
+                        )
+                    "
+        >
+          Ignore
+        </button>
+      </div>
     </div>
+
+    <p v-if="loading">Loading posts...</p>
+
+    <Post
+      v-else
+      v-for="post in posts.data"
+      :key="post.data.post_id"
+      :post="post"
+    />
+
+    <p v-if="!loading && posts.data.length < 1">No posts found!</p>
+  </div>
 </template>
 
 <script>
 import Post from "../../components/Post";
 import { mapGetters } from "vuex";
 export default {
-    name: "Show",
-    components: {
-        Post
-    },
-    data() {
-        return {
-            posts: null,
-            loading: true
-        };
-    },
-    mounted() {
-        this.$store.dispatch("fetchUser", this.$route.params.userId);
-    },
-    computed: {
-        ...mapGetters({
-            user: "user",
-            friendButtonText: "friendButtonText"
-        })
-    }
+  name: "Show",
+  components: {
+    Post
+  },
+  data() {
+    return {
+      posts: null,
+      loading: true
+    };
+  },
+  mounted() {
+    this.$store.dispatch("fetchUser", this.$route.params.userId);
+  },
+  computed: {
+    ...mapGetters({
+      user: "user",
+      friendButtonText: "friendButtonText"
+    })
+  }
 };
 </script>
 
